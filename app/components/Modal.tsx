912d06c9aabc5e58844ffe5e15a19d0553f32f41
p-mn19 <poornimamen2019@gmail.com>
2025-01-19 19:42:46 +0530

    commit

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -45,7 +45,57 @@ const Modal:React.FC<ModalProps> = ({
                 p-4
                 text-center
                 sm:p-0">
+                    <Transition.Child
+                    as="div"
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                        <div
+                        className="
+                        relative
+                        transform
+                        overflow-hidden
+                        rounded-lg
+                        bg-orange-100
+                        px-4
+                        pb-4
+                        text-left
+                        shadow-xl
+                        transition-all
+                        w-full
+                        sm:my-8
+                        sm:w-full
+                        sm:max-w-lg
+                        sm:p-6">
+                            <div
+                            className="
+                            absolute
+                            right-0
+                            top-0
+                            hidden
+                            pr-4
+                            pt-4
+                            sm:block
+                            z-10">
+                                <button
+                                type="button"
+                                className="
+                                rounded-md
+                                bg-orange-100
+                                text-orange-400
+                                hover:text-orange-500
+                                focus:outline-none
+                                focus:ring-2
+                                focus:ring-orange-500
+                                focus:ring-offset-2">
 
+                                </button>
+                            </div>
+                        </div>
+                    </Transition.Child>
                 </div>
             </Dialog>
         </Transition.Root>
