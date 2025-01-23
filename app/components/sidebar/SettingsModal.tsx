6ce307c63e35b30b9bced06dd34679bbde92c4f3
p-mn19 <poornimamen2019@gmail.com>
2025-01-23 22:11:43 +0530

    Updated SettingsModal.tsx

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { User } from "@prisma/client";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 interface SettingsModalProps {
     isOpen?:boolean;
@@ -33,6 +35,24 @@ const SettingsModal:React.FC<SettingsModalProps> = ({
             image:currentUser?.image
         }
     });
+
+    const image= watch('image');
+    const handleUpload = (result: any) =>{
+        setValue('image', result?.info?.secure_url,{
+            shouldValidate: true
+        })
+    }
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        axios.post('/api/settings',data)
+        .then(() =>{
+            router.refresh();
+            onClose();
+        })
+        .catch(() => toast.error('Something went wrong!'))
+        .finally(() => setIsLoading(false))
+    }
     return(
         <div></div>
     );
