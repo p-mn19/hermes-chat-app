a11900f34838b008800524680bfa4a2664030fef
p-mn19 <poornimamen2019@gmail.com>
2025-01-25 18:10:44 +0530

    commit

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+import Modal from "../Modal";
 
 interface SettingsModalProps {
     isOpen?:boolean;
@@ -53,8 +54,33 @@ const SettingsModal:React.FC<SettingsModalProps> = ({
         .catch(() => toast.error('Something went wrong!'))
         .finally(() => setIsLoading(false))
     }
+    //'900/10'means opacity 10
     return(
-        <div></div>
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-12">
+        <div className="border-b border-orange-900/10 pb-12"> 
+        <h2 className="
+        text-base
+        font-semibold
+        leading-7
+        text-orange-900">
+            Profile
+        </h2>
+        <p className="mt-1 text-sm leading-6 text-orange-600">
+            Edit your public information.
+        </p>
+        <div className="
+        mt-10
+        flex
+        flex-col
+        gap-y-8">
+
+        </div>
+        </div>
+        </div>
+            </form>
+        </Modal>
     );
 }
 
